b643343311882f190d71e16ae94820e836819bd2
mhdfirmansyah748 <mhdfirmansyah748@gmail.com>
2024-06-07 16:48:48 +0000

    add footer and finishing

--- main.js
+++ main.js
@@ -49,74 +49,61 @@ for (let i = 0; i < button.length; i++) {
     },
     1: {
       image: "Jokowismangroveplanting1",
-      information: " Kegiatan penanaman pohon mangrove yang dilakukan oleh Presiden Joko Widodo di Kabupaten Bengkalis, Kegiatan ini bertujuan untuk menghijaukan kawasan pesisir dan melindungi ekosistem mangrove di Bengkalis."
+      information: "Kegiatan penanaman pohon mangrove yang dilakukan oleh Presiden Joko Widodo di Kabupaten Bengkalis, Kegiatan ini bertujuan untuk menghijaukan kawasan pesisir dan melindungi ekosistem mangrove di Bengkalis."
     },
     2: {
+      image: "Jokowismangroveplanting2",
+      information: "Kegiatan penanaman pohon mangrove yang dilakukan oleh Presiden Joko Widodo di Kabupaten Bengkalis, Kegiatan ini bertujuan untuk menghijaukan kawasan pesisir dan melindungi ekosistem mangrove di Bengkalis."
+    },
+    3: {
       image: "MangroveatBukitbatu",
       information: "Hutan mangrove yang terletak di Desa Bukitbatu, Kecamatan Bengkalis. Hutan mangrove ini memiliki luas sekitar 500 hektare dan merupakan salah satu hutan mangrove terbesar di Bengkalis."
     },
-    3: {
+    4: {
       image: "Nelayan",
       information: "Aktivitas penangkapan ikan dan hasil laut lainnya yang dilakukan oleh nelayan setempat. Bengkalis merupakan salah satu daerah penghasil ikan terbesar di Indonesia."
     },
-    4: {
+    5: {
       image: "Perahujongselatbaru",
       information: "Perahu jong adalah sejenis perahu tradisional yang berasal dari daerah Selat Baru. Perahu ini dikenal dengan bentuknya yang unik dan ukurannya yang besar."
     },
-    5: {
+    6: {
+      image: "Sunsetpesisir",
+      information: "Suasana senja di pesisir bengkalis"
+    },
+    7: {
+      image: "Sunsetpesisir2",
+      information: "Suasana senja di pesisir bengkalis"
+    },
+    8: {
       image: "IstiqomahMosque2",
       information: "Masjid Istiqomah Bengkalis merupakan salah satu masjid terbesar dan tertua di Bengkalis dan menjadi salah satu ikon Kota Bengkalis."
     },
-    // 0: {
-    //   image: "ILoveBengkalis",
-    //   information: "Foto I Love Bengkalis"
-    // },
-    // 1: {
-    //   image: "IstiqomahMosque",
-    //   information: "Foto Masjid Istiqomah"
-    // },
-    // 6: {
-    //   image: "MangroveatBukitbatu2",
-    //   information: "Foto Hutan Mangrove di Bukitbatu"
-    // },
-    // 9: {
-    //   image: "Perahujong",
-    //   information: "Foto Perahu Jong"
-    // },
-  //   10: {
-  //     image: "RumahDatukLaksamanaBukitbatu",
-  //     information: "Foto Rumah Datuk Laksamana di Bukitbatu"
-  //   },
-  //   11: {
-  //     image: "Sunsetpesisir",
-  //     information: "Foto Sunset di Pesisir"
-  //   },
-  //   12: {
-  //     image: "Sunsetpesisir2",
-  //     information: "Foto Sunset di Pesisir"
-  //   }
   };
   
   const hero = document.querySelector('#content article img');
   const information = document.querySelector('.information');
-  const previeous = document.querySelector('#previous');
-  const next = document.querySelector('#next');
 
 {let index = 0;
-function changeBackgroundImage() {
-  let gambarke = images[index]["image"]
-  // let gambar = `url(assets/${gambarke}.webp)`
-  let gambar = `assets/${gambarke}.webp`
-  // hero.style.backgroundImage = gambar;
-  hero.src = gambar;
-  information.innerHTML = images[index]["information"];
-  // previeous.onclick = index -= 1;
-  next == onclick ? ++index : "";
-  console.log(index);
-  index = (index + 1);
-  index == 6 ? index = 0 : '';}
-setInterval(changeBackgroundImage, 10000);}
+  if (localStorage.getItem('nilai')) {
+    index = localStorage.getItem('nilai');
+  }
+  function changeBackgroundImage() {
+      let gambarke = images[index]["image"]
+      let gambar = `assets/${gambarke}.webp`
+      hero.src = gambar;
+      information.innerHTML = images[index]["information"];
+      console.log(index)
+      index = (index + 1);
+      if(index <= 9){
+        localStorage.setItem('nilai', index);
+      } else{}
+      index == 9 ? index = 0 : '';
+    };
+  setInterval(changeBackgroundImage, 1000);
+  };
     // hero.style.backgroundImage = 'url(assets/${images[2]["image"]}.jpg)'
+
 
 // Mapp
 document.querySelector('#m1').addEventListener('click', () => {
